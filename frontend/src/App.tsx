62e908b1c264a62ed89a3568377d02aecdc67b76
BuildTools <unconfigured@null.spigotmc.org>
2023-01-06 16:28:22 -0600

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1></h1>
+      </header>
+      <main>
+        <p>hi</p>
+      </main>
+      <footer>
+        <p>Copyright &copy; Noah Perry 2023</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
